e15c77c3c18e9246d252e2cf424b12a70fb0f2dc
Anh Tuan <tuanl799@gmail.com>
2025-05-17 00:56:06 +0700

    update new docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'thesis_dev_kit_docs'
+project = 'thesis_xiao_dev_kit'
 copyright = '2025, Tuan Nguyen'
 author = 'Tuan Nguyen'
 release = '1.0'
@@ -14,25 +14,19 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['breathe', 'exhale']
+extensions = ['breathe']
 
-# Breathe config
+templates_path = ['_templates']
+exclude_patterns = []
+
 breathe_projects = {
     "thesis_dev_kit_docs": "../../xml"
 }
 breathe_default_project = "thesis_dev_kit_docs"
 
-# Exhale config
-exhale_args = {
-    "containmentFolder": "./api",
-    "rootFileName": "library_root.rst",
-    "rootFileTitle": "API Reference",
-    "doxygenStripFromPath": "..",
-    "createTreeView": True,
-}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
